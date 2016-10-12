cc0cf0499d8819174faa5567acd9fe0f90c61d90
sachinnarang126 <sachinnarang126@gmail.com>
2016-10-12 16:46:06 +0530

    commit from dayboy

--- app/src/main/java/com/interviewquestion/presenter/SettingPresenterImpl.java
+++ app/src/main/java/com/interviewquestion/presenter/SettingPresenterImpl.java
@@ -44,7 +44,7 @@ public class SettingPresenterImpl implements SettingPresenter, Preference.OnPref
         switch (preference.getKey()) {
 
             case "prefResetAll":
-                showResetAllQuestionDialog("Do you want to Reset All Questions?", new DialogInterface.OnClickListener() {
+                showResetAllQuestionDialog("Do you want to reset all questions?", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         resetAllQuestion();
